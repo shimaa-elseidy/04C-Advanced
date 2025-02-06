039a6b3f73450c2172d296505e24fa867be4d87a
Shimaa Elseidy <shimaaelseidy123@gmail.com>
2025-02-06 12:37:02 +0200

    01 & 02 Non-Generic Collection - Hashtable

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,45 @@
-﻿namespace Demo
+﻿using System.Collections;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region HashTables category overview
+            // Collection :: Built in data structure in C#
+            // Hashtables 
+            #endregion
+            #region Non-Generic Collection - Hashtable
+            // Non-Generic Collection - Hashtable
+            // key must be unique - not null
+            //Hashtable ht = new Hashtable();
+            //ht.Add("ahmed", 3);
+            //ht.Add("ali", 5);  // maynf3sh a3ml repeat ll key
+            //ht.Add("shimaa", 0);
+            //ht.Add("amr", 33);
+            //ht.Add(33, 2);
+            //Console.WriteLine(ht.Count); // 5
+            //Console.WriteLine(ht.IsFixedSize); // false // dynamic
+            //Console.WriteLine(ht.IsReadOnly); // false
+            //ht["shimaa"] = 44;// assign new value aw law msh mawgood b set as new key & value
+            //Console.WriteLine(ht[33]); // this[] ==> take key and return value
+            //ht.Remove(null);// law b3tlha null ---> exception
+            //foreach (DictionaryEntry item in ht) // bucket -> private so i used DictionaryEntry 
+            //{
+            //    Console.WriteLine($"{item} ");
+            //}
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //foreach (var item in ht.Keys)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            //Console.WriteLine("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~");
+            //foreach (var item in ht.Values)
+            //{
+            //    Console.WriteLine(item);
+            //}
+            #endregion
         }
     }
 }
